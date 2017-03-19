999dd7a9d327b04060d4624be7260277a7906d69
Daniel Lin <Daniel_Lin@outlook.com>
2017-03-19 15:50:22 +0100

    adding eventhandlers

--- TekenApplicatie1/src/FirstController.java
+++ TekenApplicatie1/src/FirstController.java
@@ -1,11 +1,12 @@
 import drawing.domain.Drawing;
 import drawing.domain.Oval;
 import drawing.domain.Point;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-
+import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,6 +25,28 @@ public class FirstController implements Initializable {
         drawing.addDrawing(oval);
 
 
+        drawingcanvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+            }
+        });
+
+        drawingcanvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+            }
+        });
+
+        drawingcanvas.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                
+            }
+        });
+
     }
+
 
 }
